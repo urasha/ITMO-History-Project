46f50cabbb9196e4cfac9a7d7657b8df1c65fac9
Andrey Lashkul <andrejlashkul@gmail.com>
2023-12-01 17:30:39 +0300

    app.js checking

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import RegistrationPage from "./components/registration/Page";
-import GreetingPage from "./components/greetingPage/GreetingPage";
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import RegistrationPage from './components/registration/Page'
+import AbilitiesPage from './components/beginner/abilities/Page';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            {/* <RegistrationPage title="Невская застава" /> */}
-			<GreetingPage />
-            <StatusBar style="auto" />
-        </View>
-    );
+  // return (
+  //   <View style={styles.container}>
+  //     <RegistrationPage title="Невская застава"/>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
+  return (
+    <View style={styles.container}>
+      <AbilitiesPage title='Особенности'/>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
